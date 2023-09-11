d932b4cc899679a3db9a53599ac2d961dbb22588
unknown <mdmostofakhan1972@gmail.com>
2023-09-11 11:35:31 -0700

    nam

--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -5,8 +5,8 @@ import { NavLink, Link, Outlet } from "react-router-dom";
 const Dashboard = () => {
   return (
     <>
-      <div className="grid grid-flow-col">
-        <div className="bg-[#0C4E67] h-screen col-span-2">
+      <div className="flex">
+        <div className="bg-[#0C4E67] h-full col-span-2">
           <div className="my-10">
             <Link to="/" className="">
               <p className="logoFont text-[#03384D] text-center text-5xl font-bold">
@@ -20,7 +20,7 @@ const Dashboard = () => {
                 className="flex gap-2 p-4 text-xl font-semibold items-center"
                 to="/dashboard"
               >
-                <Icon icon="material-symbols:dashboard" /> Dashboard
+                <Icon icon="material-symbols:dashboard" /> <span className=" hidden md:inline-block">Dashboard</span>
               </NavLink>
             </li>
             <li className=" mt-2 text-white mb-8 ">
@@ -29,7 +29,7 @@ const Dashboard = () => {
                 to="/dashboard/orders"
               >
                 <Icon icon="iconoir:cart" />
-                Orders
+              <span>  Orders</span>
               </NavLink>
             </li>
             <li className=" mt-2 text-white mb-8 ">
@@ -38,7 +38,7 @@ const Dashboard = () => {
                 to="/dashboard/products"
               >
                 <Icon icon="streamline:shopping-bag-hand-bag-1-shopping-bag-purse-goods-item-products" />
-                Products
+               <span> Products</span>
               </NavLink>
             </li>
             <li className=" mt-2 text-white mb-8">
@@ -47,7 +47,7 @@ const Dashboard = () => {
                 to="/dashboard/addproducts"
               >
                 <Icon icon="streamline:shopping-bag-hand-bag-2-shopping-bag-purse-goods-item-products" />{" "}
-                Add Products
+               <span> Add Products</span>
               </NavLink>
             </li>
             <li className="  mt-2 text-white mb-8">
@@ -56,7 +56,7 @@ const Dashboard = () => {
                 to="/dashboard/delivery"
               >
                 <Icon icon="iconoir:delivery-truck" />
-                Delivery & Return
+              <span>  Delivery & Return</span>
               </NavLink>
             </li>
             <li className=" mt-2 text-white mb-8">
@@ -64,7 +64,7 @@ const Dashboard = () => {
                 className="flex gap-2 p-4 text-xl font-semibold items-center"
                 to="/dashboard/contact"
               >
-                <Icon icon="ri:contacts-fill" /> Contact Details
+                <Icon icon="ri:contacts-fill" /> <span> Contact Details</span>
               </NavLink>
             </li>
             <li className=" mt-2 text-white mb-8">
@@ -72,12 +72,12 @@ const Dashboard = () => {
                 className="flex gap-2 p-4 text-xl font-semibold items-center"
                 to="/dashboard/faq"
               >
-                <Icon icon="bi:chat-right-dots-fill" /> FAQ
+                <Icon icon="bi:chat-right-dots-fill" /> <span>FAQ</span>
               </NavLink>
             </li>
             <li className=" mt-2 text-white mb-8">
               <button className="flex hover:text-[#3997B1] transition-all delay-100 ease-in-out p-4 gap-2 text-xl font-semibold items-center">
-                <Icon icon="mi:log-in"/> Log Out
+                <Icon icon="mi:log-in"/> <span>Log Out</span>
               </button>
             </li>
           </ul>
--- src/pages/Dashboard/DashboardDetails.jsx
+++ src/pages/Dashboard/DashboardDetails.jsx
@@ -4,9 +4,9 @@ import ReChart from "./ReChart";
 
 const DashboardDetails = () => {
   return (
-    <div className="w-11/12 mx-auto">
+    <div className="w-11/12 mx-auto ">
       <p className="text-2xl font-black my-6">Dashboard</p>
-      <div className="flex lg:flex-nowrap md:flex-nowrap flex-wrap justify-between gap-10">
+      <div className="grid xl:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-10">
         <div className="border text-center border-green-500 rounded-lg w-full py-10">
           <p>Total Sales</p>
           <p className="text-4xl mt-3 font-semibold">৳ 75.9k</p>
@@ -24,11 +24,11 @@ const DashboardDetails = () => {
           <p className="text-4xl mt-3 font-semibold">৳ 75.9k</p>
         </div>
       </div>
-      <div className="flex gap-10 mt-10 justify-between">
-        <div className="w-full">
+      <div className="grid lg:w-full md:grid-cols-2 sm:grid-cols-1 gap-10 mt-10 ">
+        <div className="">
         <Chart></Chart>
         </div>
-        <div className="w-full">
+        <div className="">
         <ReChart></ReChart>
         </div>
       </div>
